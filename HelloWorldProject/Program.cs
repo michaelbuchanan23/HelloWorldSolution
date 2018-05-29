e02309a75211f9514acd03756129ed03a1695c65
michaelbuchanan23 <michaelbuchanan23@gmail.com>
2018-05-29 16:42:39 -0400

    initial commit

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorldProject
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
+namespace HelloWorldProject {
+	class Program {
+		static void Main(string[] args)	{
+			Console.WriteLine("Hello, world!");
+
+			Student student = new Student();
+			student.SetFirstname("Frank");
+			student.SetLastname("Zappa");
+			student.SetStudentId(123);
+			student.SetGPA(3.5);
+			student.SetSAT(1520);
+			student.SetIsFemale(false);
+
+			string fname;
+			string lname;
+			int sid;
+			double gpa;
+			int sat;
+			bool female;
+
+			fname = student.GetFirstname();
+			lname = student.GetLastname();
+			sid = student.GetStudentId();
+			gpa = student.GetGPA();
+			sat = student.GetSAT();
+			female = student.GetIsFemale();
+
+			Console.WriteLine();
+			Console.WriteLine("Name:\t" + fname + " " + lname);
+			Console.WriteLine("Id:" + "\t" + sid);
+			Console.WriteLine("GPA:\t" + gpa);
+			Console.WriteLine("SAT:\t" + sat);
+			Console.WriteLine("Female:\t" + female);
+			Console.WriteLine();
 		}
 	}
 }
---
+++ HelloWorldProject/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProject {
+	class Student {
+		string Firstname;
+		string Lastname;
+		int StudentId;
+		double GPA;
+		int SAT;
+		bool IsFemale;
+
+		//FIRST NAME
+		public string GetFirstname() {
+			return Firstname;
+		}
+		public void SetFirstname(string pFirstname) {
+			Firstname = pFirstname;
+		}
+
+		//LAST NAME
+		public string GetLastname() {
+			return Lastname;
+		}
+		public void SetLastname(string pLastname) {
+			Lastname = pLastname;
+		}
+
+		//STUDENT ID
+		public int GetStudentId() {
+			return StudentId;
+		}
+		public void SetStudentId(int pStudentId) {
+			StudentId = pStudentId;
+		}
+
+		//GPA
+		public double GetGPA() {
+			return GPA;
+		}
+		public void SetGPA(double pGPA) {
+			GPA = pGPA;
+		}
+
+		//SAT;
+		public int GetSAT() {
+			return SAT;
+		}
+		public void SetSAT(int pSAT) {
+			SAT = pSAT;
+		}
+
+
+		//IsFemale;
+		public bool GetIsFemale() {
+			return IsFemale;
+		}
+		public void SetIsFemale(bool pIsFemale) {
+			IsFemale = pIsFemale;
+		}
+	}
+}
